3b0d2dca27a35c848e8362367fa4288aaf4e6d2a
Jackson Carroll <Jack Carroll>
2021-01-22 17:00:08 -0500

    added comments

--- observerdesignpattern/HoleScoreDisplay.java
+++ observerdesignpattern/HoleScoreDisplay.java
@@ -1,21 +1,37 @@
 package observerdesignpattern;
 
+/**
+ * HoleScoreDisplay class implementing Observer class
+ * @author Jackson Carroll
+ */
 public class HoleScoreDisplay implements Observer {
     private Subject golfer;
     private int strokes;
     private int par;
 
+    /**
+     * Takes in a subject golfer then register's observer
+     * @param golfer
+     */
     public HoleScoreDisplay(Subject golfer) {
         this.golfer = golfer;
         golfer.registerObserver(this);
     }
 
+    /**
+     * Updates the strokes and par and displays the current score
+     * @author Jackson Carroll
+     */
     public void update(int strokes, int par) {
         this.strokes = strokes;
         this.par = par;
         displayCurrentScore();
     }
 
+    /**
+     * Displays the message for the golfers current hole stats
+     * @author Jackson Carroll
+     */
     private void displayCurrentScore() {
         System.out.println("\nCurrent Hole stats:\nPar: " + this.par + "\nStrokes: " + this.strokes);
 
