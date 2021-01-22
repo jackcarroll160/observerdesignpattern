d5bd0eab2a87690bccbc46385acd7d01b8025fb6
Jackson Carroll <Jack Carroll>
2021-01-22 16:53:30 -0500

    added to package

--- Observer.java
+++
@@ -1,6 +0,0 @@
-/**
- * Interface for Observer
- */
-public interface Observer {
-    public void update(int strokes, int par);
-}
--- Subject.java
+++
@@ -1,8 +0,0 @@
-/**
- * Interface for Subject
- */
-public interface Subject {
-    public void registerObserver(Observer observer);
-    public void removeObserver(Observer observer);
-    public void notifyObservers(int strokes, int par);
-}
